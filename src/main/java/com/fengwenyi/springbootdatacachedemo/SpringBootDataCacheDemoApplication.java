28ded13c034f0ac574e9d116c09bcd069e0704b2
冯文议 <xfsy_2015@163.com>
2020-10-12 10:01:00 +0800

    Initial commit

---
+++ src/main/java/com/fengwenyi/springbootdatacachedemo/SpringBootDataCacheDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.springbootdatacachedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataCacheDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDataCacheDemoApplication.class, args);
+    }
+
+}
